a211d0eccee2538647bb3a16b57a1c35a961333f
Weslie <weslie@weslie.tk>
2024-09-21 11:40:47 +0200

    add F7 Key to google Farming Mode.

--- src/client/java/tk/weslie/elementshelper/MinecraftElements2HelperClient.java
+++ src/client/java/tk/weslie/elementshelper/MinecraftElements2HelperClient.java
@@ -2,12 +2,16 @@ package tk.weslie.elementshelper;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registry;
@@ -23,6 +27,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import net.minecraft.world.event.listener.GameEventListener;
+import org.lwjgl.glfw.GLFW;
 
 
 import java.util.HashSet;
@@ -31,6 +36,10 @@ import java.util.Set;
 public class MinecraftElements2HelperClient implements ClientModInitializer  {
 	private static final Set<Block> UNBREAKABLE_BLOCKS = new HashSet<>();
 	private static final Set<Block> FARMLAND_BLOCKS = new HashSet<>();
+
+	private static KeyBinding autoHitKeybind;
+	private static long keyPressTimeout = 0;
+	private static boolean isFarmingModeEnabled = true;
 
 	static {
 		UNBREAKABLE_BLOCKS.add(Blocks.KELP_PLANT);
@@ -48,7 +57,34 @@ public class MinecraftElements2HelperClient implements ClientModInitializer  {
 
 	@Override
 	public void onInitializeClient() {
+		autoHitKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"Toggle Farming Mode",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_F7,
+				"Helper Mods"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			try{
+				if(autoHitKeybind == null)
+					return;
+
+				while (autoHitKeybind.wasPressed()) {
+					if (keyPressTimeout + 2000 < System.currentTimeMillis()) {
+						keyPressTimeout = System.currentTimeMillis();
+						isFarmingModeEnabled = !isFarmingModeEnabled;
+						client.player.sendMessage(Text.of(isFarmingModeEnabled ? "Farming-Mode aktiviert!" : "Farming-Mode deaktiviert!"), true);
+					}
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		});
+
 		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+			if(!isFarmingModeEnabled)
+				return ActionResult.PASS;
+
 			BlockState state = world.getBlockState(pos);
 
 			if(player.getMainHandStack().getItem() == Items.STICK)
