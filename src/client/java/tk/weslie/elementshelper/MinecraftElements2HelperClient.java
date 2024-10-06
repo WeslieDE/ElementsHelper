fc7e56277866788178f7cf74929bd76ec7fce796
Weslie <weslie@weslie.tk>
2024-10-06 02:11:04 +0200

    add new blocks

--- src/client/java/tk/weslie/elementshelper/MinecraftElements2HelperClient.java
+++ src/client/java/tk/weslie/elementshelper/MinecraftElements2HelperClient.java
@@ -9,10 +9,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.PotionItem;
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.lwjgl.glfw.GLFW;
 
 
@@ -32,14 +39,21 @@ public class MinecraftElements2HelperClient implements ClientModInitializer  {
 		UNBREAKABLE_BLOCKS.add(Blocks.KELP);
 		UNBREAKABLE_BLOCKS.add(Blocks.PUMPKIN_STEM);
 		UNBREAKABLE_BLOCKS.add(Blocks.ATTACHED_PUMPKIN_STEM);
-		UNBREAKABLE_BLOCKS.add(Blocks.AMETHYST_BLOCK);
+		UNBREAKABLE_BLOCKS.add(Blocks.BUDDING_AMETHYST);
 		UNBREAKABLE_BLOCKS.add(Blocks.TORCHFLOWER);
 		UNBREAKABLE_BLOCKS.add(Blocks.BAMBOO);
 		UNBREAKABLE_BLOCKS.add(Blocks.BAMBOO_SAPLING);
 
+
 		FARMLAND_BLOCKS.add(Blocks.MUD);
 		FARMLAND_BLOCKS.add(Blocks.FARMLAND);
+		FARMLAND_BLOCKS.add(Blocks.TERRACOTTA);
+		FARMLAND_BLOCKS.add(Blocks.WHITE_GLAZED_TERRACOTTA);
+		FARMLAND_BLOCKS.add(Blocks.BLUE_GLAZED_TERRACOTTA);
+		FARMLAND_BLOCKS.add(Blocks.BLACK_GLAZED_TERRACOTTA);
+		FARMLAND_BLOCKS.add(Blocks.GLASS);
 	}
+
 
 	@Override
 	public void onInitializeClient() {
